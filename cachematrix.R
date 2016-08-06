89a5b6a20d2ecd18904d231fcb468c697703d60f
dcardella.endeavorequity.com <dcardella@endeavorequity.com>
2016-08-06 13:09:08 -0400

    Week 3 Exercise

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## To establish the Matrx, type these commands:
+# x <- matrix(runif(25), 5, 5)
+# a<- makeCacheMatrix(x)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ ## makeVector <- function(x = numeric()) {
+    i <- NULL
+    set <- function(y) {
+      x <<- y
+      i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(x) i <<- solve(x)
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
+  
+## To run this function and complete the exercise, input these commands:
+# b<- cacheSolve(a)
+# x
+# b
+## And to verify that the cache is working properly, type this again:
+# cacheSolve(a)
 
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+  }
